eb555d11499cfc89d3d2b3fcf0b6f2ece3f27b3c
Coolway99 <xxcoolwayxx@gmail.com>
2015-02-08 23:33:15 -0500

    Fixed some autogen errors

--- jlime/pc/edition/GUI_Splash.java
+++ jlime/pc/edition/GUI_Splash.java
@@ -1,30 +1,25 @@
 package jlime.pc.edition;
  
-import java.awt.AlphaComposite;
 import java.awt.Color;
 import java.awt.Graphics;
-import java.awt.Graphics2D;
 import java.awt.Image;
-import java.awt.Rectangle;
-import java.awt.Toolkit;
-import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.net.URL;
 
 import javax.imageio.ImageIO;
-import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
-import javax.swing.JProgressBar;
 import javax.swing.JWindow;
 
- public class GUI_Splash extends JWindow { 
-	 URL image = getClass().getResource("assets/images/javalime.png");
+@SuppressWarnings("javadoc")
+public class GUI_Splash extends JWindow { 
+	private static final long serialVersionUID = -1825564813327818918L;
+	URL image = getClass().getResource("assets/images/javalime.png");
 	 public GUI_Splash() { 
 		 try { 
 			 setSize(480,530); 
 			 setBackground(new Color(0, 255, 0, 0));
 			 setLocationRelativeTo(null); 
-			 show(); 
+			 setVisible(true)	; 
 			 Thread.sleep(4000); 
 			 dispose();
 			 GUI z = new GUI();
@@ -32,7 +27,8 @@ import javax.swing.JWindow;
 } catch(Exception exception) { 
 	JOptionPane.showMessageDialog((java.awt.Component) null,"Error"+exception.getMessage(), "Error:", JOptionPane.DEFAULT_OPTION); 
 	} } 
-	 public void paint(Graphics g) { 
+	 @Override
+	public void paint(Graphics g) { 
 		 Image img = null;
 		
 		try {
